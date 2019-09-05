619cf718dd0ce1b04930723ec32898d53798c3ea
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-09-05 15:28:30 +0200

    Inicialización del número de versión

--- src/pages/01.InitPage.vue
+++ src/pages/01.InitPage.vue
@@ -20,7 +20,7 @@
 
             <!-- Version -->
             <div class="version">
-                <small>v.1.5.4</small>
+                <small>v.0.0.1</small>
             </div>
         </f7-toolbar>
     </f7-page>
