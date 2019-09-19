58ed0f82c80430e9383dbcefab7b4ed2cb0f403e
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-09-19 12:42:57 +0200

    Reset del estado

--- src/pages/ResetStorePage.vue
+++ src/pages/ResetStorePage.vue
@@ -6,21 +6,10 @@
     export default {
         name: "ResetStorePage",
         created() {
-            this.$store.dispatch("setAvailability", {});
-            this.$store.dispatch("setCancelList", []);
             this.$store.dispatch("setUserName", "");
             this.$store.dispatch("setUserPass", "");
-            this.$store.dispatch("setDate", "");
-            this.$store.dispatch("setDoctors", {});
-            this.$store.dispatch("setReasonsList", {});
-            this.$store.dispatch("setSchedule", {});
-            this.$store.dispatch("setSpecialty", []);
-            this.$store.dispatch("setTimeSlot", '');
-            this.$store.dispatch("setDoctor", ["", "Seleccionar..."]);
-            this.$store.dispatch("setPatient", ["", "Seleccionar..."]);
-            this.$store.dispatch("setReason", ["", "Seleccionar..."]);
-            this.$store.dispatch("setPatientsList", null);
-            this.$store.dispatch("setTimeRange", []);
+            this.$store.dispatch("setUserCode", "");
+            this.$store.dispatch("setLoadStates", {});
 
             this.$f7router.navigate('/login');
         }
