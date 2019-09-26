6f70b3ecd3566b187778143c266810348f9df51d
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-09-26 16:19:21 +0200

    Build 0.0.8

--- src/pages/01.InitPage.vue
+++ src/pages/01.InitPage.vue
@@ -20,7 +20,7 @@
 
             <!-- Version -->
             <div class="version">
-                <small>v.0.0.7</small>
+                <small>v.0.0.8</small>
             </div>
         </f7-toolbar>
     </f7-page>
