39a4a7f25b36b48fded13af2cbed9668cee7b61a
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-10-24 17:38:09 +0200

    Corregidos los enlaces

--- src/pages/panel-right.vue
+++ src/pages/panel-right.vue
@@ -4,9 +4,9 @@
         <f7-block>
             <f7-list>
                 <f7-list-item link="/home" title="Inicio" view="#main-view" panel-close></f7-list-item>
-                <f7-list-item link="/new-appointment" title="Tráfico" view="#main-view" panel-close></f7-list-item>
-                <f7-list-item link="/new-patient" title="Nota de Carga" view="#main-view" panel-close></f7-list-item>
-                <f7-list-item link="/patients-list" title="Nuevo Cliente" view="#main-view" panel-close></f7-list-item>
+                <f7-list-item link="/view-traffic" title="Tráfico" view="#main-view" panel-close></f7-list-item>
+                <f7-list-item link="/cargo-note" title="Nota de Carga" view="#main-view" panel-close></f7-list-item>
+                <f7-list-item link="/#" title="Nuevo Cliente" view="#main-view" panel-close></f7-list-item>
                 <f7-list-item link="/reset-store" title="Salir" view="#main-view" panel-close></f7-list-item>
             </f7-list>
         </f7-block>
