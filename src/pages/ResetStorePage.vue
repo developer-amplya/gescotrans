1e9a95558d65f5ac8c353e96f7d99c7b51f51b41
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-10-24 18:20:52 +0200

    Configurado el reset del estado al salir de la aplicación

--- src/pages/ResetStorePage.vue
+++ src/pages/ResetStorePage.vue
@@ -7,9 +7,38 @@
         name: "ResetStorePage",
         created() {
             this.$store.dispatch("setUserName", "");
-            this.$store.dispatch("setUserPass", "");
-            this.$store.dispatch("setUserCode", "");
+            this.$store.dispatch("setUserPass", '');
+            this.$store.dispatch("setUserCode", '');
             this.$store.dispatch("setLoadStates", {});
+            this.$store.dispatch("setSchedule", {});
+            this.$store.dispatch("setCustomersList", [
+                ['000', 'A', ''],
+                ['456', 'Antonio Orozco', '666 555 444'],
+                ['254', 'Andrea Valles', '666 777 888'],
+                ['000', 'B', ''],
+                ['965', 'Bartolome de las Chozas', '666 555 444'],
+                ['853', 'Begoña Valles', '666 777 888']
+            ]);
+            this.$store.dispatch("setServicesList", [
+                ['000', 'A', ''],
+                ['456', 'Antonio Orozco', '666 555 444'],
+                ['254', 'Andrea Valles', '666 777 888'],
+                ['000', 'B', ''],
+                ['965', 'Bartolome de las Chozas', '666 555 444'],
+                ['853', 'Begoña Valles', '666 777 888']
+            ]);
+            this.$store.dispatch("setSuppliersList", [
+                ['000', 'A', ''],
+                ['456', 'Antonio Orozco', '666 555 444'],
+                ['254', 'Andrea Valles', '666 777 888'],
+                ['000', 'B', ''],
+                ['965', 'Bartolome de las Chozas', '666 555 444'],
+                ['853', 'Begoña Valles', '666 777 888']
+            ]);
+            this.$store.dispatch("setCustomer", ['', 'Seleccionar...']);
+            this.$store.dispatch("setService", ['', 'Seleccionar...']);
+            this.$store.dispatch("setSupplier", ['', 'Seleccionar...']);
+            this.$store.dispatch("setCargoNoteDate", null);
 
             this.$f7router.navigate('/login');
         }
