3e4f55dbca309a73b400bb42b46ecc3280e3e809
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2019-12-10 18:06:50 +0100

    Se añade al proyecto por cli la plataforma y los plugins

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,10 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-add-swift-support": "2.0.2",
+  "cordova-plugin-cocoapod-support": "1.6.2"
+};
+// BOTTOM OF METADATA
+});
