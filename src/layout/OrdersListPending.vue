a50b8a2e432bba374a570291667307338d155291
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2020-05-27 10:58:38 +0200

    Se desdobla la lista de órdenes en dos: pendientes y en marcha

---
+++ src/layout/OrdersListPending.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+
+        <!-- Header -->
+        <f7-row no-gap>
+            <h3>Órdenes pendientes</h3>
+        </f7-row>
+
+        <!-- Loop -->
+        <order-item
+                v-for="(order, index) in orders"
+                :key="order.code"
+                :order="order"
+                :class="index % 2 === 0 ? 'dark' : 'light'"
+                ></order-item>
+
+    </div>
+</template>
+
+<script>
+    import OrderItem from '../layout/Order.vue';
+
+    export default {
+        name: 'OrdersList',
+        components: {
+            OrderItem
+        },
+        props: ['orders'],
+        data() {
+            return {
+                //
+            };
+        },
+        methods: {
+            //
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
