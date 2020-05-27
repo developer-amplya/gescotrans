f1cfabf90f8db946141fc31b289e60cbcf0854dc
Francisco Bosch Ruiz <contacto@franciscobosch.es>
2020-05-27 20:00:00 +0200

    Refactorización

---
+++ src/layout/OrdersList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+        <!-- Loop -->
+        <order-item
+                v-for="(order, index) in orders"
+                v-if="order[5] === 'AS'"
+                :key="order[0]"
+                :order="order"
+                :class="index % 2 === 0 ? 'dark' : 'light'"
+        ></order-item>
+
+    </div>
+</template>
+
+<script>
+    import OrderItem from './OrderItem.vue';
+
+    export default {
+        name: 'OrdersList',
+        components: {
+            OrderItem
+        },
+        props: ['orders']
+    };
+</script>
